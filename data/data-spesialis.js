ffa93b9ef0c35b2a2cbe48655561dc59e54c1c54
nether <nether.teamwork@gmail.com>
2026-09-19 09:34:19 +0800

    fixed final src foto spesialis halaman utama

--- data/data-spesialis.js
+++ data/data-spesialis.js
@@ -20,12 +20,12 @@ const dataSpesialisDokter = [
       },
       {
         nama: "Dr. Rajesh P. Shah",
-        foto: "../img/Gleneagles-doctors/dr--rajesh-p-shah.jpg",
+        foto: "img/Gleneagles-doctors/dr--rajesh-p-shah.jpg",
         link: "dokter/gleneagles.html",
       },
       {
         nama: "Dr. Annamalar Muthu",
-        foto: "../img/Gleneagles-doctors/dr--annamalar-muthu.webp",
+        foto: "img/Gleneagles-doctors/dr--annamalar-muthu.webp",
         link: "dokter/gleneagles.html",
       },
     ],
@@ -36,12 +36,12 @@ const dataSpesialisDokter = [
     dokter: [
       {
         nama: "Dato' Seri Dr. (DGPN, DSPN, PJK) (Icon) Mohamed Amir Shah",
-        foto: "../img/Island-doctors/dato'-seri-dr--mohamed-amir-shah.png",
+        foto: "img/Island-doctors/dato'-seri-dr--mohamed-amir-shah.png",
         link: "dokter/gleneagles.html",
       },
       {
         nama: "Dr. (Icon) Ooi Kai Yun",
-        foto: "../img/Island-doctors/dr--ooi-kai-yun.png",
+        foto: "img/Island-doctors/dr--ooi-kai-yun.png",
         link: "dokter/island-hospital.html",
       },
     ],
@@ -52,12 +52,12 @@ const dataSpesialisDokter = [
     dokter: [
       {
         nama: "Dr. (Icon) Tan Sui Keat",
-        foto: "../img/Island-doctors/Dr--Tan-Sui-Keat.png",
+        foto: "img/Island-doctors/Dr--Tan-Sui-Keat.png",
         link: "dokter/island-hospital.html",
       },
       {
         nama: "Dr. Leong Kin Wah",
-        foto: "../img/Gleneagles-doctors/dr--leong-kin-wah.webp",
+        foto: "img/Gleneagles-doctors/dr--leong-kin-wah.webp",
         link: "dokter/gleneagles.html",
       },
     ],
@@ -68,17 +68,17 @@ const dataSpesialisDokter = [
     dokter: [
       {
         nama: "Dr. Vijaya B. Ramasamy",
-        foto: "../img/Island-doctors/dr--vijaya-b-ramasamy.png",
+        foto: "img/Island-doctors/dr--vijaya-b-ramasamy.png",
         link: "dokter/island-hospital.html",
       },
       {
         nama: "Dato' Dr. Chen Tse Peng",
-        foto: "../img/Gleneagles-doctors/dato'-dr--chen-tse-peng.jpg",
+        foto: "img/Gleneagles-doctors/dato'-dr--chen-tse-peng.jpg",
         link: "dokter/gleneagles.html",
       },
       {
         nama: "Dr. Goh Heong Keong",
-        foto: "../img/Gleneagles-doctors/dr--goh-heong-keong.jpg",
+        foto: "img/Gleneagles-doctors/dr--goh-heong-keong.jpg",
         link: "dokter/island-hospital.html",
       },
     ],
@@ -89,17 +89,17 @@ const dataSpesialisDokter = [
     dokter: [
       {
         nama: "Dr. Lem Li Khen",
-        foto: "../img/Island-doctors/dr--lem-li-khen.png",
+        foto: "img/Island-doctors/dr--lem-li-khen.png",
         link: "dokter/island-hospital.html",
       },
       {
         nama: "Dr. P. Srinivas",
-        foto: "../img/Gleneagles-doctors/dr--p-srinivas.webp",
+        foto: "img/Gleneagles-doctors/dr--p-srinivas.webp",
         link: "dokter/gleneagles.html",
       },
       {
         nama: "Dr. Umadevi A/P A. Muthukumaru",
-        foto: "../img/Gleneagles-doctors/dr--umadevi.webp",
+        foto: "img/Gleneagles-doctors/dr--umadevi.webp",
         link: "dokter/gleneagles.html",
       },
     ],
@@ -110,12 +110,12 @@ const dataSpesialisDokter = [
     dokter: [
       {
         nama: "Dato' Dr. (DSPN) Malik Mumtaz",
-        foto: "../img/Island-doctors/Dato'-Dr--Malik-Mumtaz.png",
+        foto: "img/Island-doctors/Dato'-Dr--Malik-Mumtaz.png",
         link: "dokter/island-hospital.html",
       },
       {
         nama: "Prof. Dr. Amir S. Khir",
-        foto: "../img/Gleneagles-doctors/prof-dr--amir-s-khir.webp",
+        foto: "img/Gleneagles-doctors/prof-dr--amir-s-khir.webp",
         link: "dokter/gleneagles.html",
       },
     ],
@@ -126,17 +126,17 @@ const dataSpesialisDokter = [
     dokter: [
       {
         nama: "Dr. Goh Tiong Meng",
-        foto: "../img/Island-doctors/dr--Goh-Tiong-Meng.png",
+        foto: "img/Island-doctors/dr--Goh-Tiong-Meng.png",
         link: "dokter/island-hospital.html",
       },
       {
         nama: "Mr. Vimal K. Vasudeavan",
-        foto: "../img/Gleneagles-doctors/mr--vimal-k-vasudeavan.webp",
+        foto: "img/Gleneagles-doctors/mr--vimal-k-vasudeavan.webp",
         link: "dokter/gleneagles.html",
       },
       {
         nama: "Prof. Dr. Wong Twee Juat",
-        foto: "../img/Island-doctors/prof-dr--wong-twee-juat.png",
+        foto: "img/Island-doctors/prof-dr--wong-twee-juat.png",
         link: "dokter/island-hospital.html",
       },
     ],
@@ -147,12 +147,12 @@ const dataSpesialisDokter = [
     dokter: [
       {
         nama: "Mr. Kirubakaran A/L Malapan",
-        foto: "../img/Gleneagles-doctors/mr--kirubakaran-al-malapan.webp",
+        foto: "img/Gleneagles-doctors/mr--kirubakaran-al-malapan.webp",
         link: "dokter/gleneagles.html",
       },
       {
         nama: "Dr. (Ms.) Teh Yew Ching",
-        foto: "../img/Gleneagles-doctors/dr--teh-yew-ching.jpg",
+        foto: "img/Gleneagles-doctors/dr--teh-yew-ching.jpg",
         link: "dokter/gleneagles.html",
       },
     ],
@@ -163,22 +163,22 @@ const dataSpesialisDokter = [
     dokter: [
       {
         nama: "Dr. Mahalakshmi Ratnavale",
-        foto: "../img/Island-doctors/Dr--Mahalakshmi-Ratnavale.png",
+        foto: "img/Island-doctors/Dr--Mahalakshmi-Ratnavale.png",
         link: "dokter/island-hospital.html",
       },
       {
         nama: "Dr. Somaskandar Sivasuntharam",
-        foto: "../img/Gleneagles-doctors/dr--somaskandar-sivasuntharam.webp",
+        foto: "img/Gleneagles-doctors/dr--somaskandar-sivasuntharam.webp",
         link: "dokter/gleneagles.html",
       },
       {
         nama: "Dr. Lakshmi Paramananda",
-        foto: "../img/Gleneagles-doctors/dr--lakshmi-paramananda.webp",
+        foto: "img/Gleneagles-doctors/dr--lakshmi-paramananda.webp",
         link: "dokter/gleneagles.html",
       },
       {
         nama: "Dr. Sim Seng Keat",
-        foto: "../img/Island-doctors/Dr--Sim-Seng-Keat.png",
+        foto: "img/Island-doctors/Dr--Sim-Seng-Keat.png",
         link: "dokter/island-hospital.html",
       },
     ],
@@ -194,17 +194,17 @@ const dataSpesialisDokter = [
       },
       {
         nama: "Dato' Dr. Lai Yoon Kee",
-        foto: "../img/Gleneagles-doctors/dato'-dr--lai-yoon-kee.jpg",
+        foto: "img/Gleneagles-doctors/dato'-dr--lai-yoon-kee.jpg",
         link: "dokter/gleneagles.html",
       },
       {
         nama: "Dr. Jonathan Choon Siew Cheong",
-        foto: "../img/Gleneagles-doctors/dr--jonathan-choon-siew-cheong.jpg",
+        foto: "img/Gleneagles-doctors/dr--jonathan-choon-siew-cheong.jpg",
         link: "dokter/gleneagles.html",
       },
       {
         nama: "Dr. Amelia Lim Lay Suan",
-        foto: "../img/Gleneagles-doctors/dr--amelia-lim-lay-suan.jpeg",
+        foto: "img/Gleneagles-doctors/dr--amelia-lim-lay-suan.jpeg",
         link: "dokter/gleneagles.html",
       },
     ],
@@ -215,17 +215,17 @@ const dataSpesialisDokter = [
     dokter: [
       {
         nama: "Dr. Haniffah B. Abdul Gafoor",
-        foto: "../img/Island-doctors/dr--haniffah-b-abdul.png",
+        foto: "img/Island-doctors/dr--haniffah-b-abdul.png",
         link: "dokter/island-hospital.html",
       },
       {
         nama: "Dr. Lim Thien Thien",
-        foto: "../img/Island-doctors/Dr--Lim-Thien-Thien.png",
+        foto: "img/Island-doctors/Dr--Lim-Thien-Thien.png",
         link: "dokter/island-hospital.html",
       },
       {
         nama: "Dr. Sathindren Santhirathelagan",
-        foto: "../img/Gleneagles-doctors/dr--sathindren-santhirathelagan.jpg",
+        foto: "img/Gleneagles-doctors/dr--sathindren-santhirathelagan.jpg",
         link: "dokter/gleneagles.html",
       },
     ],
@@ -236,17 +236,17 @@ const dataSpesialisDokter = [
     dokter: [
       {
         nama: "Mr. Ooi Chong Chien",
-        foto: "../img/Gleneagles-doctors/mr--ooi-chong-chien.jpg",
+        foto: "img/Gleneagles-doctors/mr--ooi-chong-chien.jpg",
         link: "dokter/gleneagles.html",
       },
       {
         nama: "Dr. Devindran A/L Manoharan",
-        foto: "../img/Island-doctors/dr--devindran-al-manoharan.png",
+        foto: "img/Island-doctors/dr--devindran-al-manoharan.png",
         link: "dokter/island-hospital.html",
       },
       {
         nama: "Dr. S. Sritharan",
-        foto: "../img/Island-doctors/dr--s-sritharan.png",
+        foto: "img/Island-doctors/dr--s-sritharan.png",
         link: "dokter/island-hospital.html",
       },
     ],
@@ -257,17 +257,17 @@ const dataSpesialisDokter = [
     dokter: [
       {
         nama: "Dr. Jessica CG Tan",
-        foto: "../img/Gleneagles-doctors/dr--jessica-cg-tan.jpg",
+        foto: "img/Gleneagles-doctors/dr--jessica-cg-tan.jpg",
         link: "dokter/gleneagles.html",
       },
       {
         nama: "Dr. Koh Chong Tuan",
-        foto: "../img/Island-doctors/dr--koh-chong-tuan.png",
+        foto: "img/Island-doctors/dr--koh-chong-tuan.png",
         link: "dokter/island-hospital.html",
       },
       {
         nama: "Dr. Khamini Ramanujam",
-        foto: "../img/Gleneagles-doctors/dr--khamini-ramanujam.jpg",
+        foto: "img/Gleneagles-doctors/dr--khamini-ramanujam.jpg",
         link: "dokter/gleneagles.html",
       },
     ],
@@ -278,12 +278,12 @@ const dataSpesialisDokter = [
     dokter: [
       {
         nama: "Mr. K. Ravindran Katheerayson",
-        foto: "../img/Gleneagles-doctors/mr--k-ravindran-katheerayson.jpg",
+        foto: "img/Gleneagles-doctors/mr--k-ravindran-katheerayson.jpg",
         link: "dokter/gleneagles.html",
       },
       {
         nama: "Mr. Yoong Meow Foong",
-        foto: "../img/Gleneagles-doctors/mr--yoong-meow-foong.jpg",
+        foto: "img/Gleneagles-doctors/mr--yoong-meow-foong.jpg",
         link: "dokter/gleneagles.html",
       },
     ],
@@ -294,12 +294,12 @@ const dataSpesialisDokter = [
     dokter: [
       {
         nama: "Dr. Tan Bee Eng",
-        foto: "../img/Gleneagles-doctors/dr--tan-bee-eng.webp",
+        foto: "img/Gleneagles-doctors/dr--tan-bee-eng.webp",
         link: "dokter/gleneagles.html",
       },
       {
         nama: "Dr. Ng Ying Fun",
-        foto: "../img/Island-doctors/dr--ng-ying-fun.png",
+        foto: "img/Island-doctors/dr--ng-ying-fun.png",
         link: "dokter/island-hospital.html",
       },
     ],
@@ -310,17 +310,17 @@ const dataSpesialisDokter = [
     dokter: [
       {
         nama: "Dr. Oh Kim Soon",
-        foto: "../img/Island-doctors/dato'-dr--Oh-Kim-Soon.png",
+        foto: "img/Island-doctors/dato'-dr--Oh-Kim-Soon.png",
         link: "dokter/island-hospital.html",
       },
       {
         nama: "Dato' Dr. K. Parameshwaran",
-        foto: "../img/Gleneagles-doctors/dato'-dr--k-parameshwaran.webp",
+        foto: "img/Gleneagles-doctors/dato'-dr--k-parameshwaran.webp",
         link: "dokter/gleneagles.html",
       },
       {
         nama: "Mr. Khoo Eng Hooi",
-        foto: "../img/Gleneagles-doctors/mr--khoo-eng-hooi.jpg",
+        foto: "img/Gleneagles-doctors/mr--khoo-eng-hooi.jpg",
         link: "dokter/gleneagles.html",
       },
     ],
@@ -332,7 +332,7 @@ const dataSpesialisDokter = [
     dokter: [
       {
         nama: "Assoc. Prof. Dr. Edward Choke Tieng Chek",
-        foto: "../img/Northern-doctors/Assoc-Prof-Dr--Edward-Choke-Tieng-Chek.webp",
+        foto: "img/Northern-doctors/Assoc-Prof-Dr--Edward-Choke-Tieng-Chek.webp",
         link: "dokter/northern-hospital.html",
       },
     ],
