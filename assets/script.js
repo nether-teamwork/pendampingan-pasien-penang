b1e13bc5bf9a8bfef9ad68aa958dd39f860b8957
nether <nether.teamwork@gmail.com>
2026-09-22 23:24:10 +0800

    fix src

--- assets/script.js
+++ assets/script.js
@@ -282,7 +282,7 @@ document.addEventListener("DOMContentLoaded", () => {
       cardLink.innerHTML = `
           <div class="dokter-premium-card">
               <div class="avatar-frame-container">
-                  <img src="/${doc.foto}" 
+                  <img src="/img/${doc.foto}" 
                        alt="${doc.nama}" 
                        class="dokter-avatar-img" 
                        onerror="this.onerror=null; this.src='img/Logo-pendampingan-pasien-penang.png';">
